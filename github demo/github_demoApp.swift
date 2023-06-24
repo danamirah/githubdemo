7ac344bdd427e13a8f1ae319c70098d86fdeb100
dan <vanaamirah@gmail.com>
2023-06-24 13:53:15 +0400

    Initial Commit

---
+++ github demo/github_demoApp.swift
@@ -0,0 +1,17 @@
+//
+//  github_demoApp.swift
+//  github demo
+//
+//  Created by Dana on 24/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct github_demoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
